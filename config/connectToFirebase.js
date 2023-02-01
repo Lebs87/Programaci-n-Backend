9d0310dff95a402f1d9b398fb66243a5efa08416
lebs87 <lebs87@gmail.com>
2023-02-01 04:58:33 -0300

    Segunda entrega Proyecto Final

---
+++ config/connectToFirebase.js
@@ -0,0 +1,8 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./backend-pf2-credentials.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://backend-pf2.firebaseio.com'
+})
