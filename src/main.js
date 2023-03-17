335a3f7f2496f7bdbe3fa08b1fba02e707a4187e
lebs87 <lebs87@gmail.com>
2023-03-16 22:18:18 -0300

    Desafío 12

--- src/main.js
+++ src/main.js
@@ -1,57 +1,83 @@
 const { config, staticFiles } = require('../config/environment')
 const express = require('express')
-const expressSession = require('express-session')
-const MongoStore = require('connect-mongo')
-const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
-const { Server: HttpServer } = require('http')
-const { Server: Socket } = require('socket.io')
-const productRouter = require('../routes/productRouter')
-const sessionRouter = require('../routes/sessionRouter')
-const infoRouter = require('../routes/infoRouter')
-const app = express()
-const httpServer = new HttpServer(app)
-const io = new Socket(httpServer)
-const { products } = require('../class/productContainer')
-const { chats } = require('../class/chatContainer')
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(staticFiles))
-app.use(expressSession({
-  store: MongoStore.create({
-    mongoUrl: process.env.MONGOCREDENTIALSESSION,
-    mongoOptions: advancedOptions
-  }),
-  secret: 'secret-pin',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 60000
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+const baseProcces = () => {
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Proceso ${worker.process.pid} caido!`)
+    cluster.fork()
+  })
+  const expressSession = require('express-session')
+  const { Server: HttpServer } = require('http')
+  const { Server: Socket } = require('socket.io')
+  const app = express()
+  const httpServer = new HttpServer(app)
+  const io = new Socket(httpServer)
+  const productRouter = require('../routes/productRouter')
+  const sessionRouter = require('../routes/sessionRouter')
+  const infoRouter = require('../routes/infoRouter')
+  const MongoStore = require('connect-mongo')
+  const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+  const { products } = require('../class/productContainer')
+  const { chats } = require('../class/chatContainer')
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.static(staticFiles))
+  app.use(expressSession({
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGOCREDENTIALSESSION,
+      mongoOptions: advancedOptions
+    }),
+    secret: 'secret-pin',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000
+    }
+  }))
+
+  io.on('connection', async socket => {
+    console.log('Nuevo cliente conectado!')
+    socket.emit('productos', await products.getAll())
+    socket.on('update', async producto => {
+      await products.add(producto)
+      io.sockets.emit('productos', await products.getAll())
+    })
+    socket.emit('mensajes', await chats.getAll())
+    socket.on('newMsj', async mensaje => {
+      mensaje.date = new Date().toLocaleString()
+      await chats.add(mensaje)
+      io.sockets.emit('mensajes', await chats.getAll())
+    })
+  })
+
+  app.use('/session', sessionRouter)
+  app.use('/api', productRouter)
+  app.use('/info', infoRouter)
+
+  let PORT = (config.port) ? config.port : 8080
+
+  if (config.mode === 'CLUSTER') {
+    PORT = config.same === 1 ? PORT + cluster.worker.id - 1 : PORT
   }
-}))
-
-io.on('connection', async socket => {
-  console.log('Nuevo cliente conectado!')
-  socket.emit('productos', await products.getAll())
-  socket.on('update', async producto => {
-    await products.add(producto)
-    io.sockets.emit('productos', await products.getAll())
+  const server = httpServer.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
   })
-  socket.emit('mensajes', await chat.getAll());
-  socket.on('newMsj', async mensaje => {
-    mensaje.date = new Date().toLocaleString()
-    await chats.add(mensaje)
-    io.sockets.emit('mensajes', await chats.getAll());
-  })
-})
-
-app.use('/session', sessionRouter)
-
-app.use('/api', productRouter)
-
-app.use('/info', infoRouter)
-
-const PORT = (config.port) ? config.port : 8080
-const server = httpServer.listen(PORT, () => {
-  console.log(`Servidor http en el puerto ${server.address().port}`)
-})
-server.on('error', error => console.log(`Error en servidor ${error}`))
+  server.on('error', error => console.log(`Error en servidor ${error}`))
+}
+if (config.mode != 'CLUSTER') {
+  console.log('Server en modo FORK')
+  console.log('-------------------')
+  baseProcces()
+} else {
+  if (cluster.isPrimary) {
+    console.log('Server en modo CLUSTER')
+    console.log('----------------------')
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork()
+    }
+  } else {
+    baseProcces()
+  }
+}
